bb7588422de816db32900ed0aed5ab8b2589aa65
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2023-07-19 14:31:09 -0500

    finished group_by()

--- session-2023-07-19.R
+++ session-2023-07-19.R
@@ -14,7 +14,7 @@ starwars %>%
   filter(sex == 'female' | eye_color == 'brown') %>%
   count(sex)
 
-hey_look_colors <- c('red', 'blue', 'chartreuse' )
+hey_look_colors <- c('red', 'blue', 'chartreuse', 'mauve', 'purple', 'puce', 'sienna')
 
 starwars %>%
   filter(height >= 150, height <= 180)
@@ -29,3 +29,33 @@ starwars %>%
     height_in_remainder = height_in %% 12
   ) %>%
   select(name, starts_with('height'))
+
+## summarize
+
+# remember ctrl-shift-m
+
+starwars %>% 
+  summarise(n = n(), height = mean(height)) # will get NA
+
+starwars %>% 
+  summarise(n = n(), height = mean(height, na.rm = TRUE))
+# 174 mean
+
+starwars %>% filter(is.na(height)) # huh, Finn, Rey, Phasma, BB8 are NA height.
+
+## group by
+
+starwars %>%
+  group_by(species) %>%
+  summarise(n = n(), height = mean(height, na.rm = TRUE)) %>%
+  arrange(desc(n))
+
+# okay, gungans and Kaminoans and Wookiees are tall.
+
+starwars %>%
+  group_by(species, sex) %>%
+  summarise(n = n(), height = mean(height, na.rm = TRUE)) %>%
+  arrange(desc(n))
+
+# there is ungroup() too.
+
