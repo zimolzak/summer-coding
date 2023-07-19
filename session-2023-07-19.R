75540e22c502ec1cdfc0c2cf0ade38648eee7d86
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2023-07-19 14:21:16 -0500

    some filter, some mutate

--- session-2023-07-19.R
+++ session-2023-07-19.R
@@ -2,3 +2,30 @@ library(tidyverse)
 
 starwars %>%
   filter(sex == 'female', eye_color == 'brown')
+
+starwars %>%
+  count(sex)
+
+starwars %>%
+  filter(is.na(sex) | sex == 'none') %>%
+  select(name, sex) 
+
+starwars %>%
+  filter(sex == 'female' | eye_color == 'brown') %>%
+  count(sex)
+
+hey_look_colors <- c('red', 'blue', 'chartreuse' )
+
+starwars %>%
+  filter(height >= 150, height <= 180)
+
+## mutate
+
+starwars %>%
+  mutate(
+    height_in = height / 2.54,
+    height_ft = height_in / 12,
+    height_ft_integer = floor(height_ft),
+    height_in_remainder = height_in %% 12
+  ) %>%
+  select(name, starts_with('height'))
